9713264ba4b91bdc73fb339388dc286f3716219a
11as22 <ec2-user@ip-172-31-3-73.ap-northeast-1.compute.internal>
2023-06-11 08:17:44 +0000

    [add]raty

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,7 @@ class Review < ApplicationRecord
   validates :item_price, length:{maximum: 15 }
   validates :star, numericality: {
     less_than_or_equal_to: 5,
-    greater_than_or_equal_to: 1
+    greater_than_or_equal_to: 0.5
   }, presence: true
   
   belongs_to :customer
