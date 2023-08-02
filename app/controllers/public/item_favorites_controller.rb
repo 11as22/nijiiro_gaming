5b5c98da0ceab3353fd290b478aa8a81068ce0e3
11as22 <ec2-user@ip-172-31-3-73.ap-northeast-1.compute.internal>
2023-08-02 08:35:51 +0000

    favorite ajax

--- app/controllers/public/item_favorites_controller.rb
+++ app/controllers/public/item_favorites_controller.rb
@@ -2,14 +2,14 @@ class Public::ItemFavoritesController < ApplicationController
   before_action :authenticate_customer!
 
   def create
+    @item_post = ItemPost.find(params[:item_post_id])
     @item_favorite = ItemFavorite.new(customer_id: current_customer.id, item_post_id: params[:item_post_id])
     @item_favorite.save
-    redirect_to item_post_path(params[:item_post_id])
   end
   
   def destroy
-    @item_post_favorite =ItemFavorite.find_by(customer_id: current_customer.id, item_post_id: params[:item_post_id])
+    @item_post = ItemPost.find(params[:item_post_id])
+    @item_post_favorite = ItemFavorite.find_by(customer_id: current_customer.id, item_post_id: params[:item_post_id])
     @item_post_favorite.destroy
-    redirect_to item_post_path(params[:item_post_id]) 
   end
 end
